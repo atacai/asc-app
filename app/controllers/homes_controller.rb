57c1224d0660b85d770710333f1666a0387ad7b0
atacai <ata.chai10@gmail.com>
2018-12-19 09:54:51 +0700

    add function for search result

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,11 +1,38 @@
 class HomesController < ApplicationController
+	
 	def index
-		@hotels = HTTParty.get( 'https://5c08f37bea3172001389ccbd.mockapi.io/hotels/en', headers: {'Content-Type' => 'application/json'} )
+	end
+
+	def search
+		hotels = HTTParty.get( 'https://5c08f37bea3172001389ccbd.mockapi.io/hotels/en', headers: {'Content-Type' => 'application/json'} )
 
 		if params['lang'] == 'zh'
-			@hotels = HTTParty.get( 'https://5c08f37bea3172001389ccbd.mockapi.io/hotels/zh', headers: {'Content-Type' => 'application/json'} )
+			hotels = HTTParty.get( 'https://5c08f37bea3172001389ccbd.mockapi.io/hotels/zh', headers: {'Content-Type' => 'application/json'} )
 		elsif params['lang'] == 'ja'				
-			@hotels = HTTParty.get( 'https://5c08f37bea3172001389ccbd.mockapi.io/hotels/ja', headers: {'Content-Type' => 'application/json'} )
+			hotels = HTTParty.get( 'https://5c08f37bea3172001389ccbd.mockapi.io/hotels/ja', headers: {'Content-Type' => 'application/json'} )
+		end
+
+		condition = ''
+		if !params['name'].nil? && !params['name'].blank?
+			condition = condition.blank? ? "hotel['name'].downcase.include? params['name']" : " && hotel['name'].downcase.include? params['name']"
+		end
+		if !params['price'].nil? && !params['price'].blank?
+			condition = condition.blank? ? "hotel['price'] >= params['price']" : " && hotel['price'] >= params['price']"
+		end
+		if !params['address'].nil? && !params['address'].blank?
+			condition = condition.blank? ? "hotel['address'].downcase.include? params['address']" : " && hotel['address'].downcase.include? params['address']"
+		end
+		if !params['stars'].nil? && !params['stars'].blank?
+			condition = condition.blank? ? "hotel['stars'] == params['stars']" : " && hotel['stars'] == params['stars']"
+		end
+		if !params['rating'].nil? && !params['rating'].blank?
+			condition = condition.blank? ? "hotel['rating'] >= params['rating']" : " && hotel['rating'] >= params['rating']"
+		end
+
+		@results = condition.blank? ? hotels : hotels.select { |hotel| eval(condition)  }
+		
+		if params['lang'].nil?
+			params['lang'] = 'en'
 		end
 	end
 end
